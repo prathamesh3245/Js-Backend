30f7d02b868bd4543d4466a6cb47fbfff0ce9497
Prathmesh <126573912+prathamesh3245@users.noreply.github.com>
2026-03-21 19:18:43 +0530

    Api error handling and middlewares

--- src/index.js
+++ src/index.js
@@ -7,8 +7,16 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
 
+})
+.catch((error) => {
+    console.log(`MONGODB connection failed: ${error}`);
+});
 
 
 
