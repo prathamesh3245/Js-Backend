0f8ca6a7a877afeecd4b1f6abaf66f89e198bd60
Prathmesh <126573912+prathamesh3245@users.noreply.github.com>
2026-04-05 14:25:48 +0530

    login and logout controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,24 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+
+const generateAccessAndRefreshToken = async (userId) => {
+
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        user.save({ validateBeforeSave: false })
+
+        return {accessToken, refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating Refresh and Access Token")
+    }
+
+}
 const registerUser = asyncHandler( async (req, res, next) => {
     // get user details from frontend
     // validation - not empty
@@ -15,6 +33,8 @@ const registerUser = asyncHandler( async (req, res, next) => {
     // check for user creation
     // return res
 
+
+
     const {fullname, email, username, password} = req.body;
 
     console.log(`email: ${email}`);
@@ -24,7 +44,7 @@ const registerUser = asyncHandler( async (req, res, next) => {
     }
 
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username}, {email}]
     })
         
@@ -32,18 +52,25 @@ const registerUser = asyncHandler( async (req, res, next) => {
         throw new ApiError(409, "User with email or username already exists")
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // console.log(req.files);
+
+    const avatarLocalPath = req.files?.avatar ? req.files?.avatar?.[0]?.path : null;
+    // const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required!")
     }
 
-    const avatar = await uploadOnCloudinary(avatarLocalPath);
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if(!avatar){
-        throw new ApiError(400, "Avatar file is required")
+        throw new ApiError(400, "Avatar upload failed")
     }
 
     const user = await User.create({
@@ -71,4 +98,87 @@ const registerUser = asyncHandler( async (req, res, next) => {
     
 })
 
-export {registerUser}
+
+
+const loginUser = asyncHandler(async (req, res) => {
+  
+    // get user details from req.body
+    // validation - username, email
+    // find the user
+    // password check
+    // access and refresh token
+    // send cookie
+
+    const {email, password, username} = req.body
+
+    if(!username || !email){
+        throw new ApiError(400, "username or email is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if(!user){
+        throw new ApiError(404, "User does not exist!")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401,"Password incorrect")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+            .status(200)
+            .cookie("accessToken", accessToken, options)
+            .cookie("refreshToken", refreshToken, options)
+            .json(
+                new ApiResponse(
+                    200,
+                    {
+                        user: loggedInUser, accessToken, refreshToken
+                    },
+                    "User logged in Successfully" 
+                )
+            )
+})
+
+const logoutUser = asyncHandler( async (req, res) => {
+
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+           .status(200)
+           .clearCookie("accessToken", options)
+           .clearCookie("refreshToken", options)
+           .json(new ApiResponse(200, {}, "User Logged Out"))
+    
+
+})
+
+export {registerUser, loginUser, logoutUser}
--- src/index.js
+++ src/index.js
@@ -1,69 +1,14 @@
-// require('dotenv').config({path: './env'});
-import dotenv from "dotenv";
+import "dotenv/config";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-dotenv.config({
-    path: './env'
-})
 
 connectDB()
-.then(() => {
+  .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port: ${process.env.PORT}`);
-    })
-
-})
-.catch((error) => {
+      console.log(`Server is running at port: ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
     console.log(`MONGODB connection failed: ${error}`);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-( async () => {
-    try {
-        
-        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        app.on("error", (error) => {
-            console.log("ERROR: ", error);
-            throw error;
-        });
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-
-    } catch (error) {
-        console.log("Error: ", error);
-        throw err;
-    }
-})()
-*/
+  });
