f12f9f2b1e700631d752ebc1d3901225e54b0923
Prathmesh <126573912+prathamesh3245@users.noreply.github.com>
2026-04-11 12:30:06 +0530

    Add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { loginUser, registerUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, 
+        registerUser, 
+        logoutUser, 
+        refreshAccessToken, 
+        changeCurrentPassword, 
+        getCurrentUser, 
+        updateAccountDetails, 
+        updateUserAvatar, 
+        updateUserCoverImage, 
+        getUserChannelProfile, 
+        getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,4 +34,31 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refreshToken").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/get-user").get(getCurrentUser)
+router.route("/update-account").patch(updateAccountDetails) // only one user account details is getting updated
+
+
+router.route("/avatar").patch(
+    verifyJWT, 
+    upload.single("avatar"), 
+    updateUserAvatar
+)
+
+router.route("/coverImage").patch(
+    verifyJWT, 
+    upload.single("coverImage"), 
+    updateUserCoverImage
+)
+
+router.route("/c/:username").get( 
+    verifyJWT , 
+    getUserChannelProfile
+)
+
+router.route("/history").get(
+    verifyJWT, 
+    getWatchHistory
+)
+
 export default router;
